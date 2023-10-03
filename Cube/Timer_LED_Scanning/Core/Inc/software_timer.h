542d41ddc50a20e383ad5dff2c55358556a671c0
Amyra Nguyen <thanhhien.nguyen6603@gmail.com>
2023-10-04 00:30:53 +0700

    feat: led scanning with 2 leds, fixed number on display

---
+++ Cube/Timer_LED_Scanning/Core/Inc/software_timer.h
@@ -0,0 +1,21 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: Amyra Nguyen
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+/* Include */
+#include "global.h"
+/* Program define */
+
+/* Program variable */
+
+/* Program functions*/
+void init_software_timer();
+void set_timer(int index);
+void run_timer();
+#endif /* INC_SOFTWARE_TIMER_H_ */
---
+++ Cube/Timer_LED_Scanning/Core/Src/global.c
@@ -0,0 +1,9 @@
+/*
+ * global.c
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: Amyra Nguyen
+ */
+
+#include "global.h"
+
